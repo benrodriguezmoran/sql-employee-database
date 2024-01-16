29c4b63d4f2ab4cf3427b521230d1aa859f88230
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2024-01-15 20:49:52 -0500

    menu options

--- server.js
+++ server.js
@@ -8,3 +8,14 @@ const db = mysql.createConnection({
     user:   process.env.DB_USER,
     password:   process.env.DB_PASSWORD,
   });
+
+const options = [
+    "View All Employees",
+    "Add Employee",
+    "Update Employee Role",
+    "View All Roles",
+    "Add Role",
+    "View All Departments",
+    "Add Department",
+    "Quit"
+];
