bf6fc54dc73f8a127160f1942fa63311c9f036f9
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2024-01-18 18:29:39 -0500

    IO functions

--- index.js
+++ index.js
@@ -20,19 +20,21 @@ const options = [
     "Quit"
 ];
 
-function menu() {
+function init() {
     inquirer
         .prompt(
             {
-                name:"choice",
+                name:"menu",
                 type:"list",
                 message:"Choose what to do",
                 choices: options
-            }
+            })
         .then((answer) => 
             {
-                switch (answer.choice) {
+                switch (answer.menu) {
                     case "View All Employees":
+                        
+                        break;
                     case "Add Employee":
                     case "Update Employee Role":
                     case "View All Roles":
@@ -42,15 +44,71 @@ function menu() {
                     case "Quit": 
                         console.log("Quitting");
                         db.end();
+                    default: 
+                        
                 }
             })
-        )
+        
 };
+
+function addEmployee() {
+    inquirer
+        .prompt(
+            {
+                name:"firstName",
+                type:"input",
+                message:"[Adding an employee] First name:",
+            },
+            {
+                name:"lastName",
+                type:"input",
+                message:"[Adding an employee] Last name:",
+            },
+            {
+                name:"role",
+                type:"input",
+                message:"[Adding an employee] Role:",
+                choices: options
+            })
+        .then((answers) => 
+            {
+            })
+}
+
+function addDepartment(params) {
+    
+}
+
+function viewDepartment(department) {
+    inquirer
+        .prompt(
+            {
+                name:"choice",
+                type:"list",
+                message:"Choose what to do",
+                choices: employeeOptions
+            })
+        .then((answer) => 
+            {
+                queryTable(answer)
+            })
+}
+
+const queryTable = (table) => {
+    db.query(`SELECT * FROM ${table};`, (err, res) => {
+        if (err) {
+        console.error(err);
+        }
+        console.log("\n");
+        console.table(res);
+    });
+};
+
 db.connect((err) => {
     if (err) {
       console.error("Error connecting to the database: " + err.stack);
       return;
     }
     console.log("Connected to the company database");
-    menu();
+    init();
   });
